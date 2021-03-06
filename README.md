5cbd0db0106cd36e056affbfdc8b6144ada771b8
proraj9088 <rp8735685@gmail.com>
2021-03-07 00:24:05 +0530

    connection from database created
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

---
+++ README.md
@@ -0,0 +1,29 @@
+# Conduit (Medium Clone based on realworld.io)
+
+## Technologies Used
+
+
+1. NodeJ - Platform
+2. TypeScript - Programming Language
+3. Mysql - Database(you can use PostgreSQL as well)
+4. TypeORM - ORM
+
+## Database Setup
+
+1. Enter 'mysql' as admin
+
+2. Create Database, user and grant privilege
+
+```mysql
+
+CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+
+ CREATE USER 'matomo'@'localhost' IDENTIFIED WITH mysql_native_password BY 'my-strong-password-here';
+
+
+ GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'localhost';
+
+```
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import   express from 'express'
+import   express from 'express';
+import {createConnection} from "typeorm";
 
 const app = express()
 
@@ -6,7 +7,24 @@ app.get('/', (req,res) => {
     res.send("Hello World");
 })
 
-app.listen(3232, () => {
-    console.log(`Server started on http://localhost:3232`);
-})
+async function start() {
+    await createConnection({
+        type: "mysql",
+        username: 'clone',
+        password: 'clone',
+        database: 'clone',
+        entities: [],
+        synchronize: true,
+        logging: true,
+        logger: "advanced-console"
+
+    })
+    app.listen(3232, () => {
+        console.log(`Server started on http://localhost:3232`);
+    })
+}
+
+start();
+
+
 
